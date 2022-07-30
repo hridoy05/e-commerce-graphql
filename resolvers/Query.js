b7bdad2c80aa89d5d99b70046c8e9e3b10a05a6e
Hridoy Banik <hridoy.banik71@gmail.com>
2022-07-30 23:25:10 +0600

    add filter products by average rating

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,19 +1,37 @@
 exports.Query = {
-    products: (parent, {filter}, {products})=> {
-      let filteredProducts = products
-      if(filter && filter.onSale ){
-        filteredProducts = filteredProducts.filter(product => {
-           return product.onSale
-        })
+  products: (parent, { filter }, { products, reviews }) => {
+    let filteredProducts = products;
+    //filtered products
+    if (filter) {
+      const { onSale, avgRating } = filter;
+      if (onSale) {
+        filteredProducts = filteredProducts.filter((product) => {
+          return product.onSale;
+        });
       }
-      return filteredProducts
-    },
-    product:  (parent,{id}, {products})=> {
-      
-      return products.find(product => product.id === id)
-    },
-    categories: (parent, args, {categories})=> categories,
-    category: (parent, {id}, {categories})=> {
-        return categories.find(category => category.id ===id)
+
+      if ([1, 2, 3, 4, 5].includes(avgRating)) {
+        filteredProducts = filteredProducts.filter((product) => {
+          let sumRating = 0;
+          let numberOfReviews = 0;
+          reviews.forEach((review) => {
+            if (review.productId === product.id) {
+              sumRating += review.rating;
+              numberOfReviews++;
+            }
+          });
+          const avgProductRating = sumRating / numberOfReviews;
+          return avgProductRating >= avgRating;
+        });
+      }
     }
-}
+    return filteredProducts;
+  },
+  product: (parent, { id }, { products }) => {
+    return products.find((product) => product.id === id);
+  },
+  categories: (parent, args, { categories }) => categories,
+  category: (parent, { id }, { categories }) => {
+    return categories.find((category) => category.id === id);
+  },
+};
